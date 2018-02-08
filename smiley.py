086a826ed7d1aadfa2e7a17da5d0e526f63e431e
James O'donnell <james88od@gmail.com>
2018-02-08 14:26:42 +0000

    adds qr generation, qr leads to lock flipping endpoint

--- smiley.py
+++ smiley.py
@@ -4,19 +4,25 @@ from time import sleep
 import ev3dev.auto as ev3
 from PIL import Image, ImageOps
 from ev3dev.ev3 import *
+import pyqrcode
+import png
 
+#Screen context
 screen = ev3.Screen()
 
+qr = pyqrcode.create('http://ec2-18-219-63-23.us-east-2.compute.amazonaws.com/development/lock')
+#Creates a corresponding qr code, scaled for the lcd
+qr.png('qrGenerated.png', scale=2)
 
+
+#Show the qr code
 while True:
     screen.clear()
 
-    logo = Image.open('qr2.png')
+    logo = Image.open('qrGenerated.png')
     screen.image.paste(logo, (0,0))
-
 
     # Update lcd display
     screen.update()
 
-    sleep(2)
-
+    sleep(10)
