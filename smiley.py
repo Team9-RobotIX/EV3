8ac52cc1fb8c7911ccfef6f15d8c7231ae8d20e4
James O'donnell <james88od@gmail.com>
2018-03-05 10:44:18 +0000

    shorted url and remove sleep

--- smiley.py
+++ smiley.py
@@ -7,22 +7,19 @@ from ev3dev.ev3 import *
 import pyqrcode
 import png
 
-#Screen context
 screen = ev3.Screen()
 
-qr = pyqrcode.create('http://ec2-18-219-63-23.us-east-2.compute.amazonaws.com/development/lock')
-#Creates a corresponding qr code, scaled for the lcd
-qr.png('qrGenerated.png', scale=2)
+qr = pyqrcode.create('http://18.219.63.23/development/lock')
+qr.png('qrGenerated.png', scale=3)
 
-
-#Show the qr code
 while True:
     screen.clear()
 
     logo = Image.open('qrGenerated.png')
     screen.image.paste(logo, (0,0))
 
+
     # Update lcd display
     screen.update()
 
-    sleep(10)
+    #sleep(30)
